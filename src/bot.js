320274e219731c6ab085f1c150547a2f3987a041
Corey Slaven <coreyslaven@aol.com>
2017-03-29 18:27:24 -0400

    Initial Commit

--- src/bot.js
+++ src/bot.js
@@ -6,87 +6,22 @@ let http = require('http');
 
 let Twitter = new twit(config);
 
-const ranDom = function (arr) {
-  var index = Math.floor(Math.random() * arr.length);
-  return arr[index];
+const postTweet = function postTweet () {
+  Twitter.post('statuses/update', {status: '#sudo2017'}, (err, data, response) => {
+    console.log(data);
+  });
 }
 
-//retweet a random tweet
-const retweet = function () {
-  let params = {
-    q: '%40floridacomeup OR %23FloridaHipHop OR %235MMA OR %23floridarap OR %23mieuxfl OR %40MIEUX_FL OR %40CitrusRap OR mieuxfl:media',
-    result_type: 'mixed OR recent OR popular',
-    lang: 'en'
-  }
-
-  Twitter.get('search/tweets', params, function (err,data) {
-
-    let tweet = data.statuses;
-    let randomTweet = ranDom(tweet);
-
-    if (typeof randomTweet !== 'undefined') {
-      Twitter.post('statuses/retweet/:id', {
-        id: randomTweet.id_str
-      }, function (err,response) {
-        if (response) {
-          console.log('Retweeted!');
-        }
-
-        if (err) {
-          console.log('Cannot be retweeted :( something went wrong');
-        }
-      });
-    }
-    else {
-      console.log('Something went wrong while searching for tweet');
-    }
-  })
-}
-
-retweet();
-setInterval(retweet, 60000 * 60);
-
-//favorite a random tweet
-const favoriteTweet = function () {
-
-  let params = {
-    q: '%23FloridaHipHop OR %40floridacomeup OR %23floridarap OR %235MMA OR %23mieuxfl OR %40MIEUX_FL OR %40CitrusRap OR mieuxfl:media',
-    result_type: 'mixed OR recent OR popular',
-    lang: 'en'
-  }
-
-  Twitter.get('search/tweets', params, function (err, data) {
-
-    let tweet = data.statuses;
-    let randomTweet = ranDom(tweet);
-
-    if (typeof randomTweet !== 'undefined') {
-      Twitter.post('favorites/create', {
-        id: randomTweet.id_str
-      }, function (err,response) {
-        if (response) {
-          console.log('Favorited!');
-        }
-
-        if (err) {
-          console.log('Cannot be favorited :( something went wrong');
-        }
-      });
-    }
-    else {
-      console.log('Something went wrong while searching for tweet');
-    }
-  })
-}
+postTweet();
 
 
-favoriteTweet();
-setInterval(favoriteTweet, 60000 * 60);
+setInterval(postTweet, 60000);
 
 app.listen(process.env.PORT || 5000, function () {
-  console.log('Florida Music Bot is listening on port 5000');
+  console.log('#sudo2017 is listening on port 5000');
 })
 
-setInterval(() => {
+/*setInterval(() => {
   http.get("http://florida-music-bot.herokuapp.com");
 }, 180000);
+*/
--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  consumer_key: process.env.CONSUMER_KEY,
-  consumer_secret: process.env.CONSUMER_SECRET,
-  access_token: process.env.ACCESS_TOKEN,
-  access_token_secret: process.env.ACCESS_TOKEN_SECRET
+  consumer_key: 'FvhWhyUF45cMezLixVgESaYXc',
+  consumer_secret: 	'4B3VvH30ai3VJVhKjNHVDzMZws5gpfI60FSQvPRrxfhhweZgFO',
+  access_token: '847202208533950466-JADbWrmXESd9AVNWj9I9Ra1bL2d8zES',
+  access_token_secret: 'SxONlkvcgkWC55Sba68ldcf87XDMy3ilcDU0isd4ndGDa'
 }
  //wtf are these tokens I dunno
