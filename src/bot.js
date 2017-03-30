b117a05378d466deaa2dda91db3a52272356ebb0
Corey Slaven <coreyslaven@aol.com>
2017-03-29 21:04:43 -0400

    retweet function in bot.js

--- src/bot.js
+++ src/bot.js
@@ -6,6 +6,11 @@ let http = require('http');
 
 let Twitter = new twit(config);
 
+const ranDom = function (arr) {
+  let index = Math.floor(Math.random() * arr.length);
+  return arr[index];
+}
+
 const postTweet = function postTweet () {
   Twitter.post('statuses/update', {status: '#sudo2017'}, (err, data, response) => {
     console.log(data);
@@ -14,8 +19,39 @@ const postTweet = function postTweet () {
 
 postTweet();
 
+const retweet = function retweet () {
+  let params = {
+    q: '%40sudo2017 OR %23sudo2017 sudo2017:media',
+    result_type: 'mixed OR recent OR popular',
+    lang: 'en'
+  }
 
-setInterval(postTweet, 60000);
+  Twitter.get('search/tweets', params, function (err,data) {
+
+    let tweet = data.statuses;
+    let randomTweet = ranDom(tweet);
+
+    if (typeof randomTweet !== 'undefined') {
+      Twitter.post('statuses/retweet/:id', {
+        id: randomTweet.id_str
+      }, function (err,response) {
+        if (response) {
+          console.log('Retweeted!');
+        }
+
+        if (err) {
+          console.log('Cannot be retweeted :( something went wrong');
+        }
+      });
+    }
+    else {
+      console.log('Something went wrong while searching for tweet');
+    }
+  })
+}
+
+retweet();
+setInterval(retweet, 60000 * 60);
 
 app.listen(process.env.PORT || 5000, function () {
   console.log('#sudo2017 is listening on port 5000');
