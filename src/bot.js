5f29c1f8e0a8b73ab7dfb136a9032c843d85e33b
Corey Slaven <coreyslaven@aol.com>
2017-03-31 11:28:13 -0400

    Changes to bot.js

--- src/bot.js
+++ src/bot.js
@@ -13,7 +13,7 @@ const ranDom = function (arr) {
 
 const retweet = function () {
   let params = {
-    q: '%40sudo2017 OR %23sudo2017 OR sudo2017:media OR %40HackStetson or %23HackStetson',
+    q: '%40sudo2017 OR %23sudo2017 OR sudo2017:media OR %40HackStetson OR %23HackStetson',
     result_type: 'mixed OR recent OR popular',
     lang: 'en'
   }
